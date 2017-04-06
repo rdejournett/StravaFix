70fa5bf3dba057d5fa442e23bec37000c0deb97d
Rob Dejournett <rdejournett@gmail.com>
2017-04-06 11:59:21 -0400

    Initial commit

---
+++ StravaFix/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblFileName = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.chkNotStopped = New System.Windows.Forms.CheckBox()
+        Me.chkPeed = New System.Windows.Forms.CheckBox()
+        Me.bgwConvert = New System.ComponentModel.BackgroundWorker()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(185, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Drag and Drop a Strava GPX file here"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 53)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "File Dropped:"
+        '
+        'lblFileName
+        '
+        Me.lblFileName.AutoSize = True
+        Me.lblFileName.Location = New System.Drawing.Point(32, 79)
+        Me.lblFileName.Name = "lblFileName"
+        Me.lblFileName.Size = New System.Drawing.Size(0, 13)
+        Me.lblFileName.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(39, 197)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Go"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'chkNotStopped
+        '
+        Me.chkNotStopped.AutoSize = True
+        Me.chkNotStopped.Checked = True
+        Me.chkNotStopped.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkNotStopped.Location = New System.Drawing.Point(39, 165)
+        Me.chkNotStopped.Name = "chkNotStopped"
+        Me.chkNotStopped.Size = New System.Drawing.Size(161, 17)
+        Me.chkNotStopped.TabIndex = 4
+        Me.chkNotStopped.Text = "I forgot to stop Strava at end"
+        Me.chkNotStopped.UseVisualStyleBackColor = True
+        '
+        'chkPeed
+        '
+        Me.chkPeed.AutoSize = True
+        Me.chkPeed.Checked = True
+        Me.chkPeed.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkPeed.Location = New System.Drawing.Point(39, 142)
+        Me.chkPeed.Name = "chkPeed"
+        Me.chkPeed.Size = New System.Drawing.Size(112, 17)
+        Me.chkPeed.TabIndex = 5
+        Me.chkPeed.Text = "I peed during race"
+        Me.chkPeed.UseVisualStyleBackColor = True
+        '
+        'bgwConvert
+        '
+        Me.bgwConvert.WorkerReportsProgress = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 227)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(253, 22)
+        Me.ProgressBar1.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.chkPeed)
+        Me.Controls.Add(Me.chkNotStopped)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblFileName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblFileName As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents chkNotStopped As CheckBox
+    Friend WithEvents chkPeed As CheckBox
+    Friend WithEvents bgwConvert As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ProgressBar1 As ProgressBar
+End Class
---
+++ StravaFix/Form1.vb
@@ -0,0 +1,239 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.IO
+Public Class Form1
+    Private filename As String
+    Private Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles MyBase.DragDrop
+        Dim files() As String = e.Data.GetData(DataFormats.FileDrop)
+        For Each path In files
+
+            lblFileName.Text = System.IO.Path.GetFileName(path)
+            filename = path
+        Next
+    End Sub
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles MyBase.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' load the file to text
+        Cursor = Cursors.WaitCursor
+        bgwConvert.RunWorkerAsync()
+
+    End Sub
+
+    Private Sub bgwConvert_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles bgwConvert.DoWork
+        Dim filetext As String()
+        If (filename.Length = 0) Then MsgBox("You must drag a Strava gpx file")
+        filetext = System.IO.File.ReadAllLines(filename)
+        Dim linecount As Long = filetext.LongCount
+        Dim linepos As Long = 0
+
+        Dim prevtime As DateTime = Nothing
+        Dim currtime As DateTime
+        Dim newfiletext As String = ""
+        Dim peed As Boolean = False
+        Dim oldpos As Decimal = 0
+        If chkPeed.Checked Then
+
+
+            For Each line As String In filetext
+                linepos += 1
+                Dim percentdone As Decimal = CDec(linepos) / CDec(linecount) * 100
+                If percentdone > 10 And percentdone Mod 10 < 1 And percentdone > oldpos + 1 Then
+                    bgwConvert.ReportProgress(percentdone)
+                    oldpos = percentdone
+                End If
+                Dim newline = line
+                If newline.Contains("<time>") Then
+                    If prevtime = Nothing Then
+                        Dim timestart As Integer = newline.IndexOf("<time>") + 6
+                        Dim timeend As Integer = newline.LastIndexOf("</time>")
+                        prevtime = DateTime.Parse(newline.Substring(timestart, timeend - timestart))
+                    Else
+                        Dim timestart As Integer = newline.IndexOf("<time>") + 6
+                        Dim timeend As Integer = newline.LastIndexOf("</time>")
+                        currtime = DateTime.Parse(newline.Substring(timestart, timeend - timestart))
+                        If ((currtime - prevtime).TotalSeconds > 5) Then
+                            peed = True
+
+                        End If
+                        If peed Then
+                            Dim newutc As DateTime
+                            newutc = prevtime.AddSeconds(1)
+
+                            newline = "   <time>" & newutc.ToUniversalTime.ToString("yyyy-MM-ddTHH:mm:ssZ") & "</time>"
+                            prevtime = newutc
+                        Else
+                            prevtime = currtime
+                        End If
+
+                    End If
+
+                End If
+                newfiletext &= newline & vbCrLf
+            Next
+            Dim newfilename As String
+            newfilename = Path.GetDirectoryName(filename) & "\" & Path.GetFileNameWithoutExtension(filename) & "_fixed.gpx"
+            If File.Exists(newfilename) Then File.Delete(newfilename)
+            File.WriteAllText(newfilename, newfiletext)
+            MsgBox("Pee fix complete!")
+
+        End If
+
+        If chkNotStopped.Checked Then
+            Dim prevhr As Integer = 0
+            Dim currhr As Integer = 0
+            ' use the newfiletext
+            Dim newfilename As String
+            Dim atFinish = False
+            Dim delete = False
+            Dim deletepos As Long = 0
+            prevtime = Nothing
+            currtime = Nothing
+            Dim measureHR As Boolean = False
+
+            newfilename = Path.GetDirectoryName(filename) & "\" & Path.GetFileNameWithoutExtension(filename) & "_fixed.gpx"
+
+            Dim reversedFileText As String()
+
+            If chkPeed.Checked Then
+                reversedFileText = File.ReadAllLines(newfilename).Reverse.ToArray
+            Else
+                reversedFileText = File.ReadAllLines(filename).Reverse.ToArray
+            End If
+
+            linepos = 0
+            For Each line As String In reversedFileText
+                linepos += 1
+                Dim percentdone As Decimal = CDec(linepos) / CDec(linecount) * 100
+                If percentdone > 10 And percentdone Mod 10 < 1 And percentdone > oldpos + 1 Then
+                    bgwConvert.ReportProgress(percentdone)
+                    oldpos = percentdone
+                End If
+                Dim newline = line
+                If measureHR And newline.Contains("<gpxtpx:hr>") Then
+                    measureHR = False
+                    If prevhr = 0 Then
+                        Dim hrstart As Integer = newline.IndexOf("<gpxtpx:hr>") + 11
+                        Dim hrend As Integer = newline.LastIndexOf("</gpxtpx:hr>")
+                        prevhr = CInt(newline.Substring(hrstart, hrend - hrstart))
+                    Else
+                        Dim hrstart As Integer = newline.IndexOf("<gpxtpx:hr>") + 11
+                        Dim hrend As Integer = newline.LastIndexOf("</gpxtpx:hr>")
+                        currhr = CInt(newline.Substring(hrstart, hrend - hrstart))
+                        ' looking for HR to stabilize 
+                        ' first HR (prevhr) should be say 120
+                        ' next one (currhr) should be 121
+
+                        If (currhr > 140) Then
+                            If prevhr - currhr >= 1 Then
+                                atFinish = True
+                            End If
+                            ' we're going up, we've reached the end - ie finish point.
+
+                        End If
+                        prevhr = currhr
+
+                    End If
+                End If
+
+                If newline.Contains("<time>") Then
+                    If prevtime = Nothing Then
+                        Dim timestart As Integer = newline.IndexOf("<time>") + 6
+                        Dim timeend As Integer = newline.LastIndexOf("</time>")
+                        prevtime = DateTime.Parse(newline.Substring(timestart, timeend - timestart))
+                    Else
+                        Dim timestart As Integer = newline.IndexOf("<time>") + 6
+                        Dim timeend As Integer = newline.LastIndexOf("</time>")
+                        currtime = DateTime.Parse(newline.Substring(timestart, timeend - timestart))
+                        If ((prevtime - currtime).TotalSeconds > 30) Then
+                            ' measure hr
+                            measureHR = True
+                            prevtime = currtime
+                        End If
+
+
+                    End If
+                End If
+
+
+                If newline.Contains("<trkpt ") And atFinish Then
+                    ' delete from here
+                    delete = True
+                    deletepos = reversedFileText.LongCount - linepos
+                    Exit For
+                End If
+            Next
+
+            Dim writeArray() As String
+            ReDim writeArray(deletepos)
+
+            Array.Copy(reversedFileText.Reverse.ToArray, writeArray, deletepos)
+            writeArray.Add("  </trkseg>")
+            writeArray.Add(" </trk>")
+            writeArray.Add("</gpx>")
+            If File.Exists(newfilename) Then File.Delete(newfilename)
+            File.WriteAllLines(newfilename, writeArray)
+
+            '    newfiletext &= "  </trkseg>" & vbCrLf
+            '    newfiletext &= " </trk>" & vbCrLf
+            '    newfiletext &= "</gpx>" & vbCrLf
+
+
+            'newfiletext = ""
+            'Dim BufferSize As Integer = 1024
+            'Dim pos As Long = 0
+            'If deletepos > 0 Then
+            '    Dim fileToRead As String
+            '    If chkPeed.Checked Then
+            '        fileToRead = newfilename
+            '    Else
+            '        fileToRead = filename
+            '    End If
+
+
+
+            '    Using sr As StreamReader = New StreamReader(fileToRead)
+
+            '        Dim line As String = sr.ReadLine()
+            '        Do While pos < deletepos And (Not line Is Nothing)
+            '            pos += 1
+            '            newfiletext &= line & vbCrLf
+            '            line = sr.ReadLine()
+            '        Loop
+            '    End Using
+
+            '    newfiletext &= "  </trkseg>" & vbCrLf
+            '    newfiletext &= " </trk>" & vbCrLf
+            '    newfiletext &= "</gpx>" & vbCrLf
+
+            '    If File.Exists(newfilename) Then File.Delete(newfilename)
+            '    File.WriteAllText(newfilename, newfiletext)
+            'End If
+
+            MsgBox("Stop fix complete!")
+        End If
+
+    End Sub
+
+    Private Sub bgwConvert_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles bgwConvert.RunWorkerCompleted
+        Me.Cursor = Cursors.Arrow
+        'MsgBox("Complete!")
+    End Sub
+
+    Private Sub bgwConvert_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles bgwConvert.ProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
+End Class
+
+Public Module MyExtensions
+    <Extension()>
+    Public Sub Add(Of T)(ByRef arr As T(), item As T)
+        Array.Resize(arr, arr.Length + 1)
+        arr(arr.Length - 1) = item
+    End Sub
+End Module
+
